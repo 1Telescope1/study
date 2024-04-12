03b46c587b31d5baaf5ce8cf43cce7c6a0e8a30e
1Telescope1 <2298908190@qq.com>
2024-04-12 10:59:44 +0800

    feat

--- test/index.js
+++ test/index.js
@@ -1,12 +1,6 @@
-const file = new File(['Hello, World!'], 'hello.txt', { type: 'text/plain' });
+new Promise((resolve) => {
+  // resolve();
+  console.log(123);
+});
 
-const fileReader = new FileReader();
-
-fileReader.onload = (e) => {
-  // 通过 e.target?.result 获取读取的文件块内容，并将其转换为 ArrayBuffer 类型
-  const fileBuffer = e.target?.result;
-  console.log(fileBuffer); // ArrayBuffer {...}
-};
-
-// 读取文件内容
-fileReader.readAsArrayBuffer(file);
+console.log(3);
---
+++ 控制请求并发/index.js
@@ -0,0 +1,45 @@
+const requestList = [];
+for (let i = 0; i < 100; i++) {
+  const promise = () => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve('接口完成：' + i);
+      }, Math.random() * 1000);
+    });
+  };
+  requestList.push(promise);
+}
+
+const pool = new Set();
+const waitQue = [];
+function requestQue(request, max) {
+  return new Promise((resolve, reject) => {
+    if (pool.size > max) {
+      waitQue.push(request);
+    } else {
+      pool.add(request);
+      request()
+        .then((res) => {
+          console.log('done', res);
+          resolve(res);
+        })
+        .catch((err) => {
+          reject(err);
+        })
+        .finally(() => {
+          pool.delete(request);
+          console.log('等待的请求数量：', waitQue.length);
+          if (waitQue.length > 0) {
+            const nextRequest = waitQue.shift();
+            requestQue(nextRequest, max);
+          } else if (pool.size == 0) {
+            console.log('all done');
+          }
+        });
+    }
+  });
+}
+
+requestList.forEach((request) => {
+  requestQue(request, 10);
+});
